c8151b159fbaa96d9c443fd1f0e6144d406362ef
Владимир Алексеевич Очев <51778553+Ramedlav@users.noreply.github.com>
2021-03-08 17:09:39 +0100

    Update 2020_03_30_221547_create_riquests_table.php

--- database/migrations/2020_03_30_221547_create_riquests_table.php
+++ database/migrations/2020_03_30_221547_create_riquests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRequestsTable extends Migration
+class CreateRiquestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateRequestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('requests');
+        Schema::dropIfExists('riquests');
     }
 }
